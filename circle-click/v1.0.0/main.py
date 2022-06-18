d09d6bf58414e02c3617bbc8316bcda297ef305f
JWL <j.lebert4402@gmail.com>
2022-06-18 14:01:55 -0400

    created basic circle class

--- circle-click/v1.0.0/main.py
+++ circle-click/v1.0.0/main.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 class Game:
 	def __init__(self):
@@ -14,12 +16,16 @@ class Game:
 
 	def start(self):
 		self.active = True
+		self.circles = []
 
 		while self.active:
 			self.update()
 	
 	def draw(self):
 		self.WIN.fill(WHITE)
+
+		for circle in self.circles:
+			pygame.draw.circle(self.WIN, circle.color, circle.pos, circle.radius)
 
 		pygame.display.update()
 
@@ -29,6 +35,14 @@ class Game:
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				exit()
+			
+			if event.type == pygame.KEYDOWN:
+				self.handle_key_input()
+
+	def handle_key_input(self):
+		keys = pygame.key.get_pressed()
+		if keys[pygame.K_r]:
+			self.start()
 
 	def update(self):
 		self.handle_events()
@@ -36,6 +50,27 @@ class Game:
 		self.draw()
 
 		self.timer.tick(self.FPS)
+
+class Circle(pygame.sprite.Sprite):
+	def __init__(self, game):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.game = game
+		self.color = BLACK
+
+		size_min, size_max = (20, 50)
+		size = random.choice(range(size_min, size_max + 1))
+		self.image = pygame.Surface((size, size))
+		self.image.fill(self.color)
+
+		WIN_width = self.game.WIN.get_size()[0]
+		r = size // 2
+		self.radius = r
+
+		x = random.choice(range(10 + r, WIN_width - 10 - r))
+		y = r + 10
+		self.pos = (x, y)
+
 
 def main():
 	game = Game()
