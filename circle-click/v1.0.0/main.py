0e9db30c37dcec87c3a9ae7d40b65cdb815d1c41
JWL <j.lebert4402@gmail.com>
2022-06-18 14:46:47 -0400

    changed circles to a group instead of a list and ensured they dont spawn on top of each other

--- circle-click/v1.0.0/main.py
+++ circle-click/v1.0.0/main.py
@@ -12,24 +12,28 @@ class Game:
 		self.FPS = 30
 		self.timer = pygame.time.Clock()
 
+		self.circles = pygame.sprite.Group()
+
+		self.max_circles = 6
+		self.cir_chance = 0.8 / self.FPS # 0.5 / second
+
 		pygame.display.set_caption("Circle Click")
 
 	def start(self):
 		self.active = True
-		self.circles = []
 
-		self.max_circles = 6
-		self.cir_chance = 0.8 / self.FPS # 0.5 / second
+		if len(self.circles.sprites()) > 0:
+			self.circles.empty()
 
 		while self.active:
 			self.update()
 
 	def create_circle(self):
-		if not len(self.circles) < self.max_circles:
+		if not len(self.circles.sprites()) < self.max_circles:
 			return
 		
 		if random.uniform(0.0, 1.0) < self.cir_chance:
-			self.circles.append(Circle(self))
+			self.circles.add(Circle(self))
 	
 	def draw(self):
 		self.WIN.fill(WHITE)
@@ -65,13 +69,24 @@ class Game:
 
 class Circle(pygame.sprite.Sprite):
 	def __init__(self, game):
+		"""Initializes the circle class."""
 		pygame.sprite.Sprite.__init__(self)
 
 		self.game = game
 		self.color = BLACK
 
+		self.create()
+
+		# ensure no circles collide with each other
+		if len(self.game.circles.sprites()) > 0:
+			while pygame.sprite.spritecollide(self, self.game.circles, False, pygame.sprite.collide_circle):
+				self.create()
+
+	def create(self):
+		"""Creates a circle with a randomized position and size."""
 		size_min, size_max = (20, 50)
 		size = random.choice(range(size_min, size_max + 1))
+
 		self.image = pygame.Surface((size, size))
 		self.image.fill(self.color)
 
@@ -82,6 +97,8 @@ class Circle(pygame.sprite.Sprite):
 		x = random.choice(range(10 + r, WIN_width - 10 - r))
 		y = r + 10
 		self.pos = (x, y)
+
+		self.rect = self.image.get_rect(center = self.pos)
 
 
 def main():
